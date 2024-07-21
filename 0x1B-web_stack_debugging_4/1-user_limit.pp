6fb16d51c4ed8ddb58055a62623e1f06f2ac5156
jeremymordecai628 <125563448+jeremymordecai628@users.noreply.github.com>
2024-07-21 16:31:41 +0300

    Update 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,51 @@
-ble the user holberton to login and open files without error.
-
-# Increase hard file limit for Holberton user.
-exec { 'increase-hard-file-limit-for-holberton-user':
-  command => "sed -i '/^holberton hard/s/5/50000/' /etc/security/limits.conf",
-  path    => '/usr/local/bin/:/bin/'
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/',
 }
 
-# Increase soft file limit for Holberton user.
+# Reload Nginx to apply configuration changes
+exec { 'nginx-reload':
+  command     => 'nginx -s reload',
+  path        => ['/usr/bin', '/usr/sbin'],
+  refreshonly => true,
+}
+
+# Ensure the Holberton user exists
+user { 'holberton':
+  ensure     => present,
+  managehome => true,
+  home       => '/home/holberton',
+  shell      => '/bin/bash',
+}
+
+# Increase hard file limit for Holberton user
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => "sed -i '/^holberton hard/s/5/50000/' /etc/security/limits.conf",
+  path    => '/usr/local/bin/:/bin/',
+  unless  => 'grep -q "^holberton hard .* 50000" /etc/security/limits.conf',
+}
+
+# Increase soft file limit for Holberton user
 exec { 'increase-soft-file-limit-for-holberton-user':
   command => 'sed -i "/^holberton soft/s/4/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
+  path    => '/usr/local/bin/:/bin/',
+  unless  => 'grep -q "^holberton soft .* 50000" /etc/security/limits.conf',
+}
+
+# Ensure the home directory of the Holberton user is accessible
+file { '/home/holberton':
+  ensure => directory,
+  owner  => 'holberton',
+  group  => 'holberton',
+  mode   => '0755',
+}
+
+# Ensure the Holberton user can open files in their home directory
+file { '/home/holberton/testfile':
+  ensure  => present,
+  owner   => 'holberton',
+  group   => 'holberton',
+  mode    => '0644',
+  content => 'This is a test file for Holberton user.',
 }
