80aa12300c1d75a8b92443445324d22e8bd4a028
jeremymordecai628 <125563448+jeremymordecai628@users.noreply.github.com>
2024-07-21 16:48:09 +0300

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -5,6 +5,19 @@ exec { 'fix--for-nginx':
   command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/',
 }
+# Update Nginx configuration to handle 1000 connections with 100 requests at a time
+file { '/etc/nginx/nginx.conf':
+  ensure  => present,
+  content => template('nginx/nginx.conf.erb'),
+  notify  => Exec['nginx-reload'],
+}
+
+# Reload Nginx to apply configuration changes
+exec { 'nginx-reload':
+  command => 'nginx -s reload',
+  path    => ['/usr/bin', '/usr/sbin'],
+  refreshonly => true,
+}
 #->
 # Restart Nginx
 exec { 'nginx-restart':
